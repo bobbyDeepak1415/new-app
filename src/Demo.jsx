4aed1677b2937869093d2682e12983a37eeb6b2a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-06 11:11:30 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -9,18 +9,36 @@ const Demo = () => {
     "https://jsonplaceholder.typicode.com/comments",
   );
 
+const startIndex=page*Page_size
+
+  const filteredComments=allComments.slice(startIndex,startIndex+Page_size)
+
+  const handleBackClick=()=>{
+
+  }
+
+  const handleForwrdClick=()=>{
+
+  }
+
+
   return (
     <div>
       <h2>Comments</h2>
       <div>
         <ol start={page * Page_size + 1}>
-          {allComments.map((comment) => {
+          {filteredComments.map((comment) => {
             return <li key={comment.id}>{comment.name}</li>;
           })}
         </ol>
+        <button onClick={handleBackClick}>back</button>
+        <button onClick={handleForwrdClick}>Forward</button>
       </div>
     </div>
   );
 };
+
+
+
 
 export default Demo;
