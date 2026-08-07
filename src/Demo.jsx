c1d340707a46fc8e2f0c848df873f302dfef3378
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-08 02:43:17 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 
 const Demo = () => {
   return (
-    <div className='parent-container'>
-      <div className='left-container' style={{}}>
-        <p>Left Container</p>
-      </div>
-      <div>
-        <p>rightContainer</p>
-      </div>
+    <div>
+      Demo
     </div>
   )
 }
