412b911fd09b364467550944dc75ad00228fc367
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-08 02:42:56 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,50 +1,16 @@
-import React, { useState } from "react";
-import useFetchData1 from "./useFetchData1";
-
-let PAGE_SIZE = 20;
+import React from 'react'
 
 const Demo = () => {
-  const url = "https://jsonplaceholder.typicode.com/comments";
-
-  const [page, setPage] = useState(0);
-
-  const { comments } = useFetchData1(url);
-
-  let startIndex = PAGE_SIZE * page;
-
-  const currentComments = comments.slice(startIndex, startIndex + PAGE_SIZE);
-
-  const handlePrevClick = () => {
-    if (page > 0) {
-      setPage((prev) => prev - 1);
-    }
-  };
-
-  const handleNextClick = () => {
-    if (startIndex * PAGE_SIZE < comments.length) {
-      setPage((prev) => prev + 1);
-    }
-  };
-
   return (
-    <div>
-      <h2>Your comments ere:</h2>
-      <ol start={page * PAGE_SIZE}>
-        {currentComments.map((comment) => {
-          return <li key={comment.id}>{comment.name}</li>;
-        })}
-      </ol>
-      <button disabled={page === 0} onClick={handlePrevClick}>
-        prev
-      </button>
-      <button
-        disabled={startIndex * PAGE_SIZE >= comments.length}
-        onClick={handleNextClick}
-      >
-        Next
-      </button>
+    <div className='parent-container'>
+      <div className='left-container' style={{}}>
+        <p>Left Container</p>
+      </div>
+      <div>
+        <p>rightContainer</p>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Demo;
+export default Demo
