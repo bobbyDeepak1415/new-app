f3a63596a04563f5009894480486e63cdfc6cc7b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-08 02:24:35 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import useFetchData1 from "./useFetchData1";
 
 const Demo = () => {
-  return <div>Demo</div>;
+
+  const url = "https://jsonplaceholder.typicode.com/comments";
+
+  const {comments}=useFetchData1(url)
+
+  return <div>
+    <h2>Your comments ere:</h2>
+  </div>;
 };
 
 export default Demo;
---
+++ src/useFetchData1.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const useFetchData1 = (url) => {
+
+    const [comments,setComments]=useState([])
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default useFetchData1
