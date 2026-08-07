b5eb51965b8debccc705c4d4b53762265a5d457d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-08 02:34:00 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,40 +1,50 @@
 import React, { useState } from "react";
 import useFetchData1 from "./useFetchData1";
 
-
-let PAGE_SIZE=20
-
+let PAGE_SIZE = 20;
 
 const Demo = () => {
-
   const url = "https://jsonplaceholder.typicode.com/comments";
 
-  const [page,setPage]=useState(1)
+  const [page, setPage] = useState(0);
 
-  const {comments}=useFetchData1(url)
+  const { comments } = useFetchData1(url);
 
-  let startIndex=PAGE_SIZE*page
+  let startIndex = PAGE_SIZE * page;
 
-  const currentComments=comments.slice(startIndex,startIndex+PAGE_SIZE)
+  const currentComments = comments.slice(startIndex, startIndex + PAGE_SIZE);
 
-  const handlePrevClick=()=>{
+  const handlePrevClick = () => {
+    if (page > 0) {
+      setPage((prev) => prev - 1);
+    }
+  };
 
-  }
+  const handleNextClick = () => {
+    if (startIndex * PAGE_SIZE < comments.length) {
+      setPage((prev) => prev + 1);
+    }
+  };
 
-  const handleNextClick=()=>{
-    
-  }
-
-  return <div>
-    <h2>Your comments ere:</h2>
-    <ul>
-      {currentComments.map((comment)=>{
-        return <li key={comment.id}>{comment.name}</li>
-      })}
-    </ul>
-    <button disabled={page===1} onClick={handlePrevClick}>prev</button>
-    <button disabled={startIndex*PAGE_SIZE>comments.length} onClick={handleNextClick}>Next</button>
-  </div>;
+  return (
+    <div>
+      <h2>Your comments ere:</h2>
+      <ul>
+        {currentComments.map((comment) => {
+          return <li key={comment.id}>{comment.name}</li>;
+        })}
+      </ul>
+      <button disabled={page === 0} onClick={handlePrevClick}>
+        prev
+      </button>
+      <button
+        disabled={startIndex * PAGE_SIZE > comments.length}
+        onClick={handleNextClick}
+      >
+        Next
+      </button>
+    </div>
+  );
 };
 
 export default Demo;
